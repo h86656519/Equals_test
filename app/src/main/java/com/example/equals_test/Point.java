bb7b909cbcca4c0b16c55e7a01debb4691b1a51f
h86656519 <h86656519@gmail.com>
2019-05-15 19:47:49 +0800

    將point 先排x, 再排y，寫成三種作法

--- app/src/main/java/com/example/equals_test/Point.java
+++ app/src/main/java/com/example/equals_test/Point.java
@@ -5,18 +5,14 @@ import java.util.Comparator;
 import java.util.Objects;
 
 public class Point {
-    private int x;
-    private int y;
+    public int x;
+    public int y;
 
-
-
-    public void setX(int x) {
+    public Point(int x, int y) {
         this.x = x;
-    }
-
-    public void setY(int y) {
         this.y = y;
     }
+
 
     @Override
     public String toString() {
@@ -38,11 +34,18 @@ public class Point {
         return false;
     }
 
-    public int PointComparator(Point o1, Point o2) {
+    public static int PointComparator(Point o1, Point o2) {
         if (o1.x - o2.x == 0) {
             return o1.y - o2.y;
         }
         return o1.x - o2.x; //大到小
+    }
 
+    public int compareTo(Point o2) {
+        Point o1 = this;
+        if (o1.x - o2.x == 0) {
+            return o1.y - o2.y;
+        }
+        return o1.x - o2.x; //大到小
     }
 }
--- app/src/test/java/com/example/equals_test/ExampleUnitTest.java
+++ app/src/test/java/com/example/equals_test/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.equals_test;
 
+import android.widget.LinearLayout;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -60,16 +62,16 @@ public class ExampleUnitTest {
 
         System.out.println(p);
 
-        Collections.sort(p, new Comparator<Point>() {
-            //example
-            @Override
-            public int compare(Point o1, Point o2) {
-                if (o1.x - o2.x == 0) {
-                    return o1.y - o2.y;
-                }
-                return o1.x - o2.x; //大到小
-            }
-        });
+//        Collections.sort(p, new Comparator<Point>() {
+//            //example
+//            @Override
+//            public int compare(Point o1, Point o2) {
+//                if (o1.x - o2.x == 0) {
+//                    return o1.y - o2.y;
+//                }
+//                return o1.x - o2.x; //大到小
+//            }
+//        });
 
         for (Point pp : p)
             System.out.println(pp.toString());
@@ -84,36 +86,27 @@ public class ExampleUnitTest {
 
         //面積比較  topLeft, topRight, bottomLeft, bottomRight
         final List<Square> s = new ArrayList<>();
-//        s.add(new Square(new Point(2, 7), new Point(5, 7), new Point(2, 3), new Point(5, 3))); //12
-//        s.add(new Square(new Point(1, 6), new Point(4, 6), new Point(1, 2), new Point(4, 2))); //12
-//        s.add(new Square(new Point(1, 5), new Point(3, 4), new Point(1, 2), new Point(3, 2))); //6
-//        s.add(new Square(new Point(1, 4), new Point(4, 4), new Point(1, 2), new Point(4, 2))); //6
-//        s.add(new Square(new Point(1, 5), new Point(4, 5), new Point(1, 2), new Point(4, 2))); //9
-        Point point1 = new Point();
-        Point point2 = new Point();
-        Point point3 = new Point();
-        Point point4 = new Point();
-        point1.setX(2);
-        point1.setY(7);
-        point2.setX(5);
-        point2.setY(7);
-        point3.setX(2);
-        point3.setY(3);
-        point4.setX(5);
-        point4.setY(3);
-        s.add(new Square(point1, point2, point3, point4)); //12
-
+        s.add(new Square(new Point(2, 7), new Point(5, 7), new Point(2, 3), new Point(5, 3))); //12
+        s.add(new Square(new Point(1, 6), new Point(4, 6), new Point(1, 2), new Point(4, 2))); //12
+        s.add(new Square(new Point(1, 5), new Point(3, 4), new Point(1, 2), new Point(3, 2))); //6
+        s.add(new Square(new Point(1, 4), new Point(4, 4), new Point(1, 2), new Point(4, 2))); //6
+        s.add(new Square(new Point(1, 5), new Point(4, 5), new Point(1, 2), new Point(4, 2))); //9
 
         Collections.sort(s, new Comparator<Square>() {
             @Override
             public int compare(Square o1, Square o2) {
                 if (o1.SquareArea() - o2.SquareArea() == 0) {
+//                    作法一:
 //                    if (o1.getTopLeft().x - o2.getTopLeft().x == 0) {
 //                        return o1.getTopLeft().x - o2.getTopLeft().x;
 //                    }
-                    Point point = new Point();
-                    point.PointComparator(o1.getTopLeft().x, o2.getTopLeft().x);
-                    return o1.getTopLeft().y - o2.getTopLeft().y;
+
+//                    作法二: static 的做法比較差
+//                    int p = Point.PointComparator(o1.getTopLeft(), o2.getTopLeft());
+//                    return p;
+
+//                    作法三:
+                    return o1.getTopLeft().compareTo(o2.getTopLeft());
                 }
                 return o1.SquareArea() - o2.SquareArea(); //o1 - o2 = 升冪
 //                return o2.SquareArea() - o1.SquareArea();   //降冪
@@ -124,6 +117,28 @@ public class ExampleUnitTest {
             System.out.println(ss.SquareArea());
             System.out.println(ss.getTopLeft());
         }
+
+    }
+
+    @Test
+    public void name() {
+        Point point1 = new Point(1, 2);
+        Point point2 = new Point(3, 2);
+        Point point3 = new Point(5, 2);
+        Point point4 = new Point(6, 2);
+
+        List<Point> points = new ArrayList<>();
+        points.add(point1);
+        points.add(point2);
+        points.add(point3);
+        points.add(point4);
+
+        Collections.sort(points, new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                return o1.compareTo(o2);
+            }
+        });
 
     }
 }
